293a32ab7e4a7cfe89f2f3cf0c667ddd080a0070
Mason Settergren <masonsettergren@gmail.com>
2020-03-17 15:21:56 -0700

    Hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// Helloworld.Java
+/* Text-printing program.
+  This comment is shown as a traditional comment. */
+
+public class HelloWorld
+{
+  // main method begins execution of Java application
+  public static void main( String[] args )
+  {
+    /** @return nothing
+      * @param String */
+    System.out.println( "Hello World" );
+  } // end main method
+} // end Welcome class
+
