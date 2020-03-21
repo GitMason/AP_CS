630d024603914cab7f305b59845b57db178f0a6c
Mason Settergren <masonsettergren@gmail.com>
2020-03-21 11:42:30 -0700

    Modified Hello World (with fun bonus notes in the comments)

---
+++ ModifiedHelloWorld.java
@@ -0,0 +1,39 @@
+// Helloworld.Java
+
+// Assignment 11: Hello World
+// Assignment 19: Modified Hello World
+
+/* Text-printing program.
+  This comment is shown as a traditional comment. */
+
+public class ModifiedHelloWorld
+{
+  // main method begins execution of Java application
+  public static void main( String[] args )
+  {
+    /** @return nothing
+      * @param String */
+    
+    
+    // VARIABLES
+    
+    String text = "Mason is cool!";
+    String text2 = new String( "Mason is awesome" );
+    String text3;
+    text3 = "I love this course";
+    // All 3 ways are valid, but the first method is easiest
+    
+    int number;
+    number = 100;
+    int number2 = 123;
+    // Ditto
+    
+
+    //PRINT STATEMENTS
+    
+    System.out.println( "This course\n\nis awesome!");
+    // Two \n's because we need a blank line in between
+    
+  } // end main method
+} // end ModifiedHelloWorld class
+
