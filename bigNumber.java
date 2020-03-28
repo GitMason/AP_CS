04018a46345f9ac4aa7d627594b67a2b63c56ddc
Mason Settergren <masonsettergren@gmail.com>
2020-03-27 20:11:13 -0700

    Biiiiiiig number

---
+++ bigNumber.java
@@ -0,0 +1,26 @@
+// bigNumber.java
+// Assignment 39: Big Number Program
+
+import java.util.Scanner;
+
+public class bigNumber
+{
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    int num;
+    
+    System.out.println("Gimme a number (no decimals please) ");
+    num = s.nextInt();
+    
+    if (num > 100)
+    {
+      System.out.println("That's a pretty big number you got there. Good job.");
+    }
+    
+    else 
+    {
+      System.out.println("That's not very big. You could do better pal.");
+    }
+  }
+}
