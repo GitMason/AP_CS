5d77fea34da5be8113d754f638ac387d9b1a3314
Mason Settergren <masonsettergren@gmail.com>
2020-04-03 15:42:45 -0700

    DiceRoll.java

---
+++ DiceRoll.java
@@ -0,0 +1,11 @@
+//DiceRoll.java
+//Assignment 70: Rolling Dice
+
+public class DiceRoll
+{
+  public static void main(String[] args)
+  {
+    int x = (int)(Math.random()*6)+1;
+    System.out.println(x);
+  }
+}
